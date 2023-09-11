8bf042b5f74989b1b5695c686ce8b4f275179967
66MHassan <muhammadhassan662000@gmail.com>
2023-09-10 22:50:21 -0700

    Fix path issues in App File

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import bootstrap from 'bootstrap'
 import './App.css';
-import PostCreation from './PostCreation';
+import PostCreation from './components/PostCreation';
 
 function App() {
   return (

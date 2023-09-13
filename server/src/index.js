90466dc490df424f69f610a9eb48a767976b7b18
66MHassan <muhammadhassan662000@gmail.com>
2023-09-12 22:54:17 -0700

    Design Database ,  include mysql module in express and connected with database

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,40 @@
 const express = require('express');
 const cors=require('cors');
 const bodyparser = require('body-parser');
+const mysql =require ('mysql');
 const app=express();
 const port=8000;
 
 app.use(cors())
 app.use(bodyparser.json())
 
+const db=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'blog'
+})
+
+db.connect((err)=>{
+    if(err){
+        console.log(err);
+    }
+    console.log("Database is connected successfuly....");
+})
+
 app.post("/creatingPost",(req,res)=>{
     const title=req.body.title;
     const content=req.body.content;
     const tags=req.body.tags;
     const category=req.body.category;
-    console.log(title)
+
+    db.query('insert into post(title,content,tags) values (?,?,?)',[title,content,tags],(err,result)=>{
+        if(err){
+            console.log(err);
+        }
+        console.log(result);
+    })
+
 })
 
 
@@ -22,3 +44,5 @@ app.post("/creatingPost",(req,res)=>{
 app.listen(port,()=>{
     console.log(`Server is listening at Port : ${port}`);
 })
+
+module.exports =index;

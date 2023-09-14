2589d891d265053440a55fc2b156ef0afa35cddf
66MHassan <muhammadhassan662000@gmail.com>
2023-09-14 11:08:56 -0700

    Added pages directory and Added pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import logo from './logo.svg';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import bootstrap from 'bootstrap'
 import './App.css';
-import PostCreation from './components/PostCreation';
-import ListData from './components/ListData';
-import Home from './components/Home';
+import PostCreation from './pages/PostCreation';
+import ListData from './pages/ListData';
+import Home from './pages/Home';
 import NavBar from './components/NavBar';
 
 function App() {

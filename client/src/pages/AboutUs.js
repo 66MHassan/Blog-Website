c906a01607e02e005f3b9381479518835dfa9832
66MHassan <muhammadhassan662000@gmail.com>
2023-09-20 21:42:30 +0500

    Updated About us page

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -3,6 +3,7 @@ export default function AboutUs(){
     return(
         <>
         <h1>Hi this is About Us page</h1>
+        <label>Email:</label><h2>muhammadhassan662000@gmail.com</h2>
         </>
     )
 }

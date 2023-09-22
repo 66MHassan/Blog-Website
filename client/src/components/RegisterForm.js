ec270d0fdd218e12c4658b5c2cc8001e82aac5ce
66MHassan <muhammadhassan662000@gmail.com>
2023-09-22 21:08:45 +0500

    worked in registration Form

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -2,6 +2,10 @@ export default function RegisterForm(){
     return(
         <>
         <h1>This is Register Form</h1>
+        <form>
+            <lable>FullName:</lable><input type="text" name="fname" id="" />
+
+        </form>
         </>
     )
 }

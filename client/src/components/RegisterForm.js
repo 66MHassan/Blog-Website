9c1be2c69c92f56274f8cea149a473493a3ce342
66MHassan <muhammadhassan662000@gmail.com>
2023-09-22 21:11:49 +0500

    updated register form

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -4,6 +4,8 @@ export default function RegisterForm(){
         <h1>This is Register Form</h1>
         <form>
             <lable>FullName:</lable><input type="text" name="fname" id="" />
+            <label htmlFor="">Email:</label><input type="email" name="" id="" />
+            <label>Password:</label><input type="password" name="" id="" />
 
         </form>
         </>
